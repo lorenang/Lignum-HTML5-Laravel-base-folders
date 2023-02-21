05c32bbb8ebe09c45caf3e8e329f884153fbc53c
Lorena Gomez <lorena.natali18@gmail.com>
2023-02-21 10:41:21 -0300

    Practico 1

--- 01-JS-DOM-APIs/Practica 1/assets/js/app.js
+++ 01-JS-DOM-APIs/Practica 1/assets/js/app.js
@@ -1,8 +1,19 @@
 //Cuando la página termine de cargarse, la section debe aparecer gradualmente (fade in).
 
 window.addEventListener('load', function() {
-    setTimeout(() => {
-        document.querySelector(".section-hidden").style.display = 'block';
-      }, 900);
-    ;
+  setTimeout(() => {
+    document.querySelector(".section-hidden").style.display = 'block';
+    }, 500);
+  ;
 })
+
+/*
+ escuchar los posibles eventos de animación. Este código configura nuestros detectores de eventos
+ (event listeners); los llamamos cuando el documento carga por primera vez para configurar todo.
+
+var e = document.querySelector(".section-hidden");
+e.addEventListener("animationstart", listener, false);
+e.addEventListener("animationend", listener, false);
+e.className = "fadein";
+
+*/
